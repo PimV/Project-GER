61e653d3a9b5b96a148884e962da59dbb9847324
JohanBeekers <johan.beekers@gmail.com>
2013-11-26 15:02:27 +0100

    default head komt in template erbij.

--- View/Inc_DefaultHead.php
+++
@@ -1,7 +0,0 @@
-<title>GER</title>
-<meta charset="UTF-8"/>
-<meta name="description" content="GER (Green Engineering Rubrics) beoordeling systeem"/>
-<meta name="keywords" content="GER, student, beoordeling"/>
-<link rel="stylesheet" type="text/css" href="<?php echo(GlobalSettings::getRootFolder()) ?>Styles/TemplateStyle.css"/>
-<link rel="icon" href="<?php echo(GlobalSettings::getRootFolder()) ?>favicon.ico"/>
-<script src="<?php echo (globalsettings::getrootfolder()."Libraries/jquery-1.10.2.min.js")?>"></script>
--- View/Template.php
+++ View/Template.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <!-- Libraries -->
+        <script src="<?php echo (globalsettings::getrootfolder()."Libraries/jquery-1.10.2.min.js")?>"></script>
+        
         <!-- Head overrides & additions -->
         <?php if (isset($pagehead)){include_once($pagehead);} ?>
         
         <!-- Default head -->
-        <?php include_once 'Inc_DefaultHead.php'; ?>
+        <title>GER</title>
+        <meta charset="UTF-8"/>
+        <meta name="description" content="GER (Green Engineering Rubrics) beoordeling systeem"/>
+        <meta name="keywords" content="GER, student, beoordeling"/>
+        <link rel="stylesheet" type="text/css" href="<?php echo(GlobalSettings::getRootFolder()) ?>Styles/TemplateStyle.css"/>
+        <link rel="icon" href="<?php echo(GlobalSettings::getRootFolder()) ?>favicon.ico"/>
         
     </head>
     <body>
