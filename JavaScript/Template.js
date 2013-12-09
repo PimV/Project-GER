3c56f49a00de73bea977742c928219f9bb6fa65c
JohanBeekers <johan.beekers@gmail.com>
2013-12-09 19:20:17 +0100

    Template.js aanpassing. Het is nu mogelijk om in javascript met "getSelectedItemId()" het ID van de momenteel geselecteerde item op te vragen. Gebruik zoiets op de add/delete knoppen: onclick="javascript:location.href='index.php?p=klas&del='+getSelectedItemId();" Wanneer er niks geselecteerd is resulteerd dit in een javascript fout, en gaat hij niet verder. Wat mooi is.

--- JavaScript/Template.js
+++ JavaScript/Template.js
@@ -1,13 +1,16 @@
+var currentlySelected;
+
 $( document ).ready(function() {
     
     //Makes it possible to select an item in the item lists.
-    var currentlySelected;
     $( "tbody tr" ).click(function() {
         $( currentlySelected ).css({
             "color": "",
             "background-color": ""
         });
+        
         currentlySelected = this;
+        
         $( this ).css({
             "color": "#ffffff",
             "background-color": "#008080"
@@ -15,3 +18,7 @@ $( document ).ready(function() {
     });
     
 });
+
+function getSelectedItemId(){
+    return currentlySelected.id;
+}
