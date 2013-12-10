e025d7723cf34918d1b34c001f8f1f1167e265c4
Bas van den Heuvel <Bas van den Heuvel@bas-pc>
2013-12-10 19:33:52 +0100

    2 klassen ophaal methodes in klassen.php aangepast

--- Model/Klassen.php
+++ Model/Klassen.php
@@ -63,33 +63,51 @@ class Klassen {
     
     //2 = Pak alle klassen met een ingevulde deadline die nog NIET voorbij is (docent beoordeling)
     //mag de regel met IS NOT NULL niet gewoon weg?
-    public function getAllClassesReviewing_array(){
+    public function getAllClassesReviewing_array($studentId){
         $query = "SELECT k.id, k.klascode, b.naam, COUNT(s.id) AS studenten
                     FROM klas k 
                     LEFT JOIN klas_student s ON s.klas_id = k.id 
                     LEFT JOIN blok b ON b.id = k.blok_id
-                    WHERE k.verwijderd = false 
-                    AND beoordeling_deadline IS NOT NULL
+                    WHERE k.verwijderd = false ";
+        
+        if(!is_null($studentId)) {
+            $query .= "AND s.student_id = ? ";
+        }
+        
+        $query .= "AND beoordeling_deadline IS NOT NULL
                     AND beoordeling_deadline > NOW()
                     GROUP BY k.id";
+        if(!is_null($studentId)) {
+            $result = DatabaseConnector::executeQuery($query, array($studentId));
+        }
+        else{
+            $result = DatabaseConnector::executeQuery($query);            
+        }
         
-        $result = DatabaseConnector::executeQuery($query);
         return $result; 
     }
     //3 = pak alle klassen waar docent coach is met een ingevulde deadline die WEL voorbij is (coach)
     //mag de regel met IS NOT NULL niet gewoon weg?
-    public function getAllClassesRating_array($coachId){
+    public function getAllClassesRating_array($coachId, $studentId){
+        $parameters = array($coachId);
+        
         $query = "SELECT k.id, k.klascode, b.naam, COUNT(s.id) AS studenten
                     FROM klas k 
                     LEFT JOIN klas_student s ON s.klas_id = k.id 
                     LEFT JOIN blok b ON b.id = k.blok_id
-                    WHERE k.verwijderd = false 
-                    AND k.coach_id = ?
-                    AND beoordeling_deadline IS NOT NULL
+                    WHERE k.verwijderd = false
+                    AND k.coach_id = ? "; 
+        
+        if(!is_null($studentId)) {
+            $query .= "AND s.student_id = ? ";
+            array_push($parameters, $studentId);
+        }
+        
+        $query .= "AND beoordeling_deadline IS NOT NULL
                     AND beoordeling_deadline < NOW()
                     GROUP BY k.id";
         
-        $result = DatabaseConnector::executeQuery($query, array($coachId));
+        $result = DatabaseConnector::executeQuery($query, array($parameters));
         return $result; 
     }
         
