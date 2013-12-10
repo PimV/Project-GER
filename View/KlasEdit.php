919fc4449c8a95a65b9b5bea1b2dc95d34f703de
JohanBeekers <johan.beekers@gmail.com>
2013-12-10 23:17:29 +0100

    klasEdit transfer-list werkend.

--- View/KlasEdit.php
+++ View/KlasEdit.php
@@ -65,40 +65,42 @@
 <div style="clear: both">          
     <h2>Studenten</h2>
 
-    <ul class="listView">
-        <li class="listItem">12542 | Henk de Rooij</li>      
-        <li class="listItem">12532 | Jop Flop</li>
-    </ul>     
+    <div alt="listItem">
+        <ul class="listView" alt="left">
+            <li class="listItem">12542 | Henk de Rooij</li>      
+            <li class="listItem">12532 | Jop Flop</li>
+        </ul>     
 
-    <div class="listViewControl">
-        <div class="fontIcon">&#xe111;</div>    
-        <div class="fontIcon">&#xe112;</div>      
-    </div>    
+        <div class="listViewControl">
+            <div name="Left" class="fontIcon">&#xe111;</div>    
+            <div name="Right" class="fontIcon">&#xe112;</div>      
+        </div>    
 
-    <ul class="listView">                               
-        <li class="listItem">12543 | Bob Boppers</li>  
-        <li class="listItem">12544 | Lisa de Rooij</li>  
-        <li class="listItem">12545 | Tim de Vrij</li>  
-        <li class="listItem">12546 | Ton van Vloed</li>  
-        <li class="listItem">12547 | Max Imuns</li>  
-        <li class="listItem">12548 | Rens Snert</li>     
-        <li class="listItem">12543 | Bob Boppers</li>  
-        <li class="listItem">12544 | Lisa de Rooij</li>  
-        <li class="listItem">12545 | Tim de Vrij</li>  
-        <li class="listItem">12546 | Ton van Vloed</li>  
-        <li class="listItem">12547 | Max Imuns</li>  
-        <li class="listItem">12548 | Rens Snert</li>      
-        <li class="listItem">12543 | Bob Boppers</li>  
-        <li class="listItem">12544 | Lisa de Rooij</li>  
-        <li class="listItem">12545 | Tim de Vrij</li>  
-        <li class="listItem">12546 | Ton van Vloed</li>  
-        <li class="listItem">12547 | Max Imuns</li>  
-        <li class="listItem">12548 | Rens Snert</li>     
-        <li class="listItem">12543 | Bob Boppers</li>  
-        <li class="listItem">12544 | Lisa de Rooij</li>  
-        <li class="listItem">12545 | Tim de Vrij</li>  
-        <li class="listItem">12546 | Ton van Vloed</li>  
-        <li class="listItem">12547 | Max Imuns</li>  
-        <li class="listItem">12548 | Rens Snert</li>  
-    </ul>  
+        <ul class="listView" alt="right">                               
+            <li class="listItem">12543 | Bob Boppers</li>  
+            <li class="listItem">12544 | Lisa de Rooij</li>  
+            <li class="listItem">12545 | Tim de Vrij</li>  
+            <li class="listItem">12546 | Ton van Vloed</li>  
+            <li class="listItem">12547 | Max Imuns</li>  
+            <li class="listItem">12548 | Rens Snert</li>     
+            <li class="listItem">12543 | Bob Boppers</li>  
+            <li class="listItem">12544 | Lisa de Rooij</li>  
+            <li class="listItem">12545 | Tim de Vrij</li>  
+            <li class="listItem">12546 | Ton van Vloed</li>  
+            <li class="listItem">12547 | Max Imuns</li>  
+            <li class="listItem">12548 | Rens Snert</li>      
+            <li class="listItem">12543 | Bob Boppers</li>  
+            <li class="listItem">12544 | Lisa de Rooij</li>  
+            <li class="listItem">12545 | Tim de Vrij</li>  
+            <li class="listItem">12546 | Ton van Vloed</li>  
+            <li class="listItem">12547 | Max Imuns</li>  
+            <li class="listItem">12548 | Rens Snert</li>     
+            <li class="listItem">12543 | Bob Boppers</li>  
+            <li class="listItem">12544 | Lisa de Rooij</li>  
+            <li class="listItem">12545 | Tim de Vrij</li>  
+            <li class="listItem">12546 | Ton van Vloed</li>  
+            <li class="listItem">12547 | Max Imuns</li>  
+            <li class="listItem">12548 | Rens Snert</li>  
+        </ul> 
+    </div>
 </div>  
