246ca0b90c2a25ef40a7ab46b692707f187e8943
JohanBeekers <johan.beekers@gmail.com>
2013-12-10 20:19:46 +0100

    commentaar weggegooid uit klassen model.

--- Model/Klassen.php
+++ Model/Klassen.php
@@ -17,31 +17,6 @@ class Klassen {
     public function getAllClasses() {
         
     }
-    //    public function getAllClasses_array($currentlyReviewing = false, $noHistory = true, $coachId = null) {
-    //                
-    //        $query = "SELECT k.id, klascode, b.naam, COUNT(s.id) AS studenten
-    //                    FROM klas k 
-    //                    LEFT JOIN klas_student s ON s.klas_id = k.id 
-    //                    LEFT JOIN blok b ON b.id = k.blok_id
-    //                    WHERE k.verwijderd = false ";
-    //        
-    //        if($currentlyReviewing) {
-    //            $query .= "AND beoordeling_deadline IS NOT NULL 
-    //                        AND beoordeling_deadline > NOW() ";
-    //        }
-    //        else {
-    //            if($noHistory) {
-    //                $query .= "AND (beoordeling_deadline IS NULL 
-    //                            OR beoordeling_deadline < NOW()) ";
-    //            }
-    //        }
-    //        
-    //        $query .= "GROUP BY k.id";
-    //        
-    //        $result = DatabaseConnector::executeQuery($query);
-    //        return $result;        
-    //        
-    //    }
 
     //1 = pak alle klassen (admin) 
     public function getAllClasses_array($noHistory = true){
