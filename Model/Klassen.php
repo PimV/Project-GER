38d7c2de8b7f93a90f3884481a9d738f32c5a40b
Bas van den Heuvel <Bas van den Heuvel@bas-pc>
2013-12-10 17:03:12 +0100

    klassen model aangepast, methodes aangemaakt.

--- Model/Klassen.php
+++ Model/Klassen.php
@@ -25,30 +25,77 @@ class Klassen {
      * @param boolean $noHistory If true, will not retrieve classes from the past. (reviewing deadline has already past)
      * @return array[][]
      */
-    public function getAllClasses_array($currentlyReviewing = false, $noHistory = true) {
-                
-        $query = "SELECT k.id, klascode, b.naam, COUNT(s.id) AS studenten
+    
+//    public function getAllClasses_array($currentlyReviewing = false, $noHistory = true, $coachId = null) {
+//                
+//        $query = "SELECT k.id, klascode, b.naam, COUNT(s.id) AS studenten
+//                    FROM klas k 
+//                    LEFT JOIN klas_student s ON s.klas_id = k.id 
+//                    LEFT JOIN blok b ON b.id = k.blok_id
+//                    WHERE k.verwijderd = false ";
+//        
+//        if($currentlyReviewing) {
+//            $query .= "AND beoordeling_deadline IS NOT NULL 
+//                        AND beoordeling_deadline > NOW() ";
+//        }
+//        else {
+//            if($noHistory) {
+//                $query .= "AND (beoordeling_deadline IS NULL 
+//                            OR beoordeling_deadline < NOW()) ";
+//            }
+//        }
+//        
+//        $query .= "GROUP BY k.id";
+//        
+//        $result = DatabaseConnector::executeQuery($query);
+//        return $result;        
+//        
+//    }
+    
+    
+    //1 = pak alle klassen (admin) 
+    public function getAllClasses_array(){
+        $query = "SELECT k.id, k.klascode, b.naam, COUNT(s.id) AS studenten
                     FROM klas k 
                     LEFT JOIN klas_student s ON s.klas_id = k.id 
                     LEFT JOIN blok b ON b.id = k.blok_id
-                    WHERE k.verwijderd = false ";
-        
-        if($currentlyReviewing) {
-            $query .= "AND beoordeling_deadline IS NOT NULL 
-                        AND beoordeling_deadline > NOW() ";
-        }
-        else {
-            if($noHistory) {
-                $query .= "AND (beoordeling_deadline IS NULL 
-                            OR beoordeling_deadline < NOW()) ";
-            }
-        }
-        
-        $query .= "GROUP BY k.id";
+                    WHERE k.verwijderd = false 
+                    GROUP BY k.id";
         
         $result = DatabaseConnector::executeQuery($query);
-        return $result;        
+        return $result; 
+    }
+    
+    //2 = Pak alle klassen met een ingevulde deadline die nog NIET voorbij is (docent beoordeling)
+    //mag de regel met IS NOT NULL niet gewoon weg?
+    public function getAllClassesReviewing_array(){
+        $query = "SELECT k.id, k.klascode, b.naam, COUNT(s.id) AS studenten
+                    FROM klas k 
+                    LEFT JOIN klas_student s ON s.klas_id = k.id 
+                    LEFT JOIN blok b ON b.id = k.blok_id
+                    WHERE k.verwijderd = false 
+                    AND beoordeling_deadline IS NOT NULL
+                    AND beoordeling_deadline > NOW()
+                    GROUP BY k.id";
         
+        $result = DatabaseConnector::executeQuery($query);
+        return $result; 
+    }
+    //3 = pak alle klassen waar docent coach is met een ingevulde deadline die WEL voorbij is (coach)
+    //mag de regel met IS NOT NULL niet gewoon weg?
+    public function getAllClassesRating_array($coachId){
+        $query = "SELECT k.id, k.klascode, b.naam, COUNT(s.id) AS studenten
+                    FROM klas k 
+                    LEFT JOIN klas_student s ON s.klas_id = k.id 
+                    LEFT JOIN blok b ON b.id = k.blok_id
+                    WHERE k.verwijderd = false 
+                    AND k.coach_id = ?
+                    AND beoordeling_deadline IS NOT NULL
+                    AND beoordeling_deadline < NOW()
+                    GROUP BY k.id";
+        
+        $result = DatabaseConnector::executeQuery($query, array($coachId));
+        return $result; 
     }
     
     public function getClass($classID) {
