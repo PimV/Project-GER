b7b310c8560eb12ab05ebe12da515ab63a90932a
Gijs van der Venne <gijs.venne@hotmail.com>
2013-12-17 16:32:01 +0100

    inlogin systeem "hora"

--- Model/GlobalSettings.php
+++ Model/GlobalSettings.php
@@ -5,33 +5,40 @@
  *
  * @author Johan Beekers
  */
-class GlobalSettings {
+class GlobalSettings
+    {
+
     static private $titlePrefix = "GER - ";                         //A prefix to put in the tab title.
     static private $DatabaseLocation = "5.199.151.136:3306";            //URL of the database.
     static private $DatabaseName = "mydb";                          //Name of the database.
     static private $DatabaseLogin = "PimVerlangen";                 //Login name to access the database.
     static private $DatabasePassword = "verlangen";                 //Database login password.
 
-    static public function gettitlePrefix() {
+    static public function gettitlePrefix()
+        {
         return self::$titlePrefix;
-    }
+        }
 
-    static public function getDatabaseLocation() {
+    static public function getDatabaseLocation()
+        {
         return self::$DatabaseLocation;
-    }
+        }
 
-    static public function getDatabaseName() {
+    static public function getDatabaseName()
+        {
         return self::$DatabaseName;
-    }
+        }
 
-    static public function getDatabaseLogin() {
+    static public function getDatabaseLogin()
+        {
         return self::$DatabaseLogin;
-    }
+        }
 
-    static public function getDatabasePassword() {
+    static public function getDatabasePassword()
+        {
         return self::$DatabasePassword;
-    }
+        }
 
-}
+    }
 
 ?>
