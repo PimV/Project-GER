e15a109f0ac1780c180b3760f024a72e45b7b557
JohanBeekers <johan.beekers@gmail.com>
2013-12-21 22:35:27 +0100

    Login check, home buttons permissies, sommige buttons verplaatst.

--- View/Home.php
+++ View/Home.php
@@ -1,10 +1,11 @@
 <h1>Home</h1>       
 <div class="beheerMenu">
+    <?php if($_SESSION["admin"]) { ?>
     <div class="menuRow">
         <div class="menuItem" onclick="javascript:location.href = 'index.php?p=docent'">
             <div class="fontIcon">&#xe075;</div>      
             <div class="text">Docenten</div>
-        </div>      
+        </div>
         <div class="menuItem" onclick="javascript:location.href = 'index.php?p=groep'">
             <div class="fontIcon">&#xe006;</div>      
             <div class="text">Rollen</div>
@@ -12,18 +13,16 @@
         <div class="menuItem" onclick="javascript:location.href = 'index.php?p=rubriek'">
             <div class="fontIcon">&#xe096;</div>      
             <div class="text">Rubrieken</div>
-        </div>   
-        <div class="menuItem" onclick="javascript:location.href = 'index.php?p=beoordeling'">
-            <div class="fontIcon">&#xe023;</div>      
-            <div class="text">Beoordelingen</div>
-        </div>    
-        <div class="menuItem" onclick="javascript:location.href = 'index.php?p=blok'">
-            <div class="fontIcon">&#xe02d;</div>      
-            <div class="text">Blokken</div>
+        </div>
+        <div class="menuItem" onclick="javascript:location.href = 'index.php?p=waardering'">
+            <div class="fontIcon">&#xe098;</div>      
+            <div class="text">Waarderingen</div>
         </div>
     </div>  
+    <?php } ?>
 
-    <div class="menuRow">  
+    <div class="menuRow">
+        <?php if($_SESSION["admin"]) { ?>
         <div class="menuItem" onclick="javascript:location.href = 'index.php?p=studentsearch'">
             <div class="fontIcon">&#xe070;</div>      
             <div class="text">Studenten</div>
@@ -32,22 +31,31 @@
             <div class="fontIcon">&#xe071;</div>      
             <div class="text">Klassen</div>
         </div>
+        <div class="menuItem" onclick="javascript:location.href = 'index.php?p=blok'">
+            <div class="fontIcon">&#xe02d;</div>      
+            <div class="text">Blokken</div>
+        </div>
+        <?php } ?>
         <!--TODO:-->
-        <!--CHECK OF DE DOCENT DIT JAAR COACH IS VAN EEN KLAS, ANDERS DEZE KNOP NIET EENS LATEN ZIEN BIJ EEN DOCENT!!!! DAN HEEFT DIE AAN BEOORDELING GENOEG-->
+        <!--CHECK OF DE DOCENT VAN MINSTENS ÉÉN KLAS, ANDERS DEZE KNOP NIET EENS LATEN ZIEN BIJ EEN DOCENT!!!! DAN HEEFT DIE AAN BEOORDELING GENOEG-->
+        <?php if(!$_SESSION["admin"]) { ?>
         <div class="menuItem" onclick="javascript:location.href = 'index.php?p=studentsearch'">
             <div class="fontIcon">&#xe095;</div>      
             <div class="text">Resultaten</div>
-        </div>   
-        <div class="menuItem" onclick="javascript:location.href = 'index.php?p=waardering'">
-            <div class="fontIcon">&#xe098;</div>      
-            <div class="text">Waarderingen</div>
-        </div>  
+        </div>
+        <?php } ?>
+        <div class="menuItem" onclick="javascript:location.href = 'index.php?p=beoordeling'">
+            <div class="fontIcon">&#xe023;</div>      
+            <div class="text">Beoordelingen</div>
+        </div>
     </div>     
 
-    <div class="menuRow">  
+    <div class="menuRow">
+        <?php if($_SESSION["admin"]) { ?>
         <div class="menuItem">
             <div class="fontIcon" onclick="javascript:location.href = 'index.php?p=profiel'">&#xe087;</div>      
             <div class="text">Profiel</div>
-        </div>       
+        </div>
+        <?php } ?>
     </div>
 </div>
--- index.php
+++ index.php
@@ -4,17 +4,30 @@
 //Start the session.
 session_start();
 
-//Sessie admin variable welke op true staat als volgens de DB de ingelogde een admin level heeft
+if(!isset($_SESSION['loggedin'])) {
+    $_SESSION['loggedin'] = false; //True wanneer er een gebruiker is ingelogd.
+    $_SESSION['username'] = NULL;
+    $_SESSION['admin'] = false; //True wanneer een admin is ingelogd.
+    $_SESSION['docentId'] = NULL; //Beschikbaar wanneer docent is ingelogd. Om onder andere zijn coach klassen op te halen.
+}
+
+//TODO: verwijder tijdelijke test waardes
+$_SESSION['loggedin'] = true;
+$_SESSION['username'] = "Admin+docent testUser";
 $_SESSION["admin"] = true;
-//Docent id in sessie opslaan als het om een docent gaat, hebben we nodig op resultaten pagina (voor coach)
 $_SESSION["docentId"] = 2;
 
 include_once 'Model/GlobalSettings.php';            //Static class with global settings.
 include_once 'Controller/DatabaseConnector.php';    //Static class for database connections
+
 //Get the page to open. Homepage default if none specified. 
 if (isset($_GET["p"]) && !empty($_GET["p"])) {
     $page = strtolower($_GET["p"]);
 } else {
+    $page = "home";
+}
+
+if(!$_SESSION['loggedin']) {
     $page = "login";
 }
 
