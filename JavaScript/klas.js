5f9146031abe1eab5f346ea6aee5184a9e081b79
JohanBeekers <johan.beekers@gmail.com>
2013-12-21 19:46:48 +0100

    Alert weggehaald

--- JavaScript/klas.js
+++ JavaScript/klas.js
@@ -1,6 +1,5 @@
 function deleteClass() {
     var id = getSelectedItemId();
-    alert($("#"+id+" td").last().text().length);
     if($("#"+id+" td").last().text().length) {
         showError("Deze klas staat momenteel open voor beoordelingen en kan daarom niet verwijderd worden.");
     }
