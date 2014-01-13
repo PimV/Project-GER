c1495a7c971e2430ec608598d25b6c5b04200a72
Bas van den Heuvel <Bas van den Heuvel@bas-pc>
2014-01-13 12:30:25 +0100

    Blok kolom toegevoegd op beoordeling pagina.

--- Model/Klassen.php
+++ Model/Klassen.php
@@ -124,9 +124,10 @@ class Klassen {
 
     public function getAllClassesNoResult_array() {
 
-        $query = "  SELECT k.id AS id, k.klascode AS klascode, COUNT(s.id) AS studenten
+        $query = "  SELECT k.id AS id, k.klascode AS klascode, COUNT(s.id) AS studenten, b.naam AS blok
                     FROM klas k 
-                    LEFT JOIN klas_student s ON s.klas_id = k.id 
+                    LEFT JOIN klas_student s ON s.klas_id = k.id
+                    LEFT JOIN blok b ON b.id = k.blok_id
                     WHERE beoordeling_deadline > NOW()
                     GROUP BY k.id";
 
